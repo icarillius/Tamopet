4b50dbcc26731d450b675b1454640a8e61137a46
Ian Griffiths <icarillius@gmail.com>
2025-11-27 18:49:57 +0000

    Update for TracebookmarkBM insights

--- enc_temp_folder/59abef59c9a772341d27999ca35f1df6/HungerSubsystem.cpp
+++ enc_temp_folder/59abef59c9a772341d27999ca35f1df6/HungerSubsystem.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "HungerSubsystem.h"
 
 void UHungerSubsystem::Tick(float DeltaTime)
@@ -45,9 +46,6 @@ int UHungerSubsystem::IncreaseHunger()
 	// output for increase of hunger
 	if (GEngine)
 		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT(" Increase Hunger: %i"), hunger));
-	TRACE_BOOKMARK(TEXT("IncreaseHungerBM"));
-
-
 
 	return hunger;
 }
